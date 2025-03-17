48ac66d1a1a2336a5768295978f67a7caea9ae94
Andrea Protani <113997087+Prot10@users.noreply.github.com>
2025-03-17 23:27:10 +0100

    nuovo config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Assicurati che sia attivo se usi il file-based routing
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
